b7eb1adaef0b599afc7567fdfa30c463d6059c97
Amha16 <capamha@gmail.com>
2022-11-30 15:27:03 +0300

    cart button popup added when adding amount

--- src/componets/UI/HeaderCartButton.js
+++ src/componets/UI/HeaderCartButton.js
@@ -1,16 +1,34 @@
-import { useContext } from "react";
+import { useContext,useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "./Carticon";
 import CartContext from "../../store/cart-context";
 const HeaderCartButtton = (props) => {
     const CartCtx = useContext(CartContext);
-  
+
     const numberOfCartItem = CartCtx.items.reduce((curNumber, item)=>{
         return curNumber + item.amount;
     },0)
-  
+
+    const { items } = CartCtx;
+    const [btnIsHighLighted, setBtnIsHighlighted] = useState(false);
+
+
+    const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump: '' }`;
+    useEffect(()=>{
+      if(items.lenght === 0){
+        return;
+      }
+      setBtnIsHighlighted(true);
+
+      const timer = setTimeout(()=>{
+        setBtnIsHighlighted(false);
+      }, 300);
+      return ()=> {
+        clearTimeout(timer)
+      }
+    },[items])
     return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
